855ae4fd25f5c28f09a857e412739b0fb5959414
Luke Li <lukeli1011@gmail.com>
2026-04-28 16:27:08 -0700

    Added crawler set up changes

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -1,4 +1,5 @@
 from utils import get_logger
+from collections import Counter
 from crawler.frontier import Frontier
 from crawler.worker import Worker
 
@@ -9,6 +10,20 @@ class Crawler(object):
         self.frontier = frontier_factory(config, restart)
         self.workers = list()
         self.worker_factory = worker_factory
+
+        #report statistics
+        self.total_word_count = Counter()
+        self.longest_page = {}
+        self.total_unique_pages = set()
+        self.total_sub_domains = []
+
+    def return_report_stats(self):
+        num_unique_pages = len(self.total_unique_pages)
+        longest_page = self.longest_page[0]
+        top_50 = self.total_word_count.most_common(50)
+        subdomain_list = self.total_sub_domains.sort()
+    
+        return num_unique_pages, longest_page, top_50, subdomain_list
 
     def start_async(self):
         self.workers = [
