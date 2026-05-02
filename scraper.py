5311934d8b5f7aafe6d881e45e441208a7d2266f
Luke Li <lukeli1011@gmail.com>
2026-05-01 17:24:06 -0700

    Fix scraper print bug

--- scraper.py
+++ scraper.py
@@ -32,7 +32,7 @@ url_stats = {}          # {subdomain: [num_unique_pages, {path1: num1, path2, nu
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     report_stats = [unique_pages, longest_page, word_frequencies, subdomains]
-    if resp == 200:
+    if resp.status == 200:
         print("Scraping", url)
     return [link for link in links if is_valid(link)], report_stats
 
